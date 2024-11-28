af5d6fbdc419ba5a7b92d864e1d6311f3fecc0f4
Azila Wandani <azilawandani@student.uir.ac.id>
2024-11-28 13:36:07 +0700

    first commit

---
+++ src/layouting.js
@@ -0,0 +1,9 @@
+export const FLEX_CENTER = {
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+  
+  export const FLEX_ROW_SPACE_BETWEEN = {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  };
